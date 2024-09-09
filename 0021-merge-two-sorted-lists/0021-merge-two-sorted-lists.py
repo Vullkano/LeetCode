dd216a6cd3a35e86a82573f18a1b8f42be93cbc2
Diogo Alonso Freitas <115744397+Vullkano@users.noreply.github.com>
2024-09-09 18:33:48 +0100

    Time: 47 ms (5.69%), Space: 16.5 MB (34.82%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,31 +5,45 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        dummyHead = ListNode()
-        tail = dummyHead
         
-        while list1 or list2:
+        if not list1:
             
-            if list1 is None:
-                digit1 = float('inf')
-            else:
-                digit1 = list1.val
+            if not list2:
+                return None
             
-
-            if list2 is None:
-                digit2 = float('inf')
             else:
-                digit2 = list2.val
-
-
-            if digit1 < digit2:
-                newNode = ListNode(digit1)
-                list1 = list1.next
+                return list2
+        else:
+            
+            if not list2:
+                return list1
+            
             else:
-                newNode = ListNode(digit2)
-                list2 = list2.next
+                dummyHead = ListNode()
+                tail = dummyHead
+                
+                while list1 or list2:
+                    
+                    if list1 is None:
+                        digit1 = float('inf')
+                    else:
+                        digit1 = list1.val
+                    
 
-            tail.next = newNode
-            tail = tail.next
-        
-        return dummyHead.next
+                    if list2 is None:
+                        digit2 = float('inf')
+                    else:
+                        digit2 = list2.val
+
+
+                    if digit1 < digit2:
+                        newNode = ListNode(digit1)
+                        list1 = list1.next
+                    else:
+                        newNode = ListNode(digit2)
+                        list2 = list2.next
+
+                    tail.next = newNode
+                    tail = tail.next
+                
+                return dummyHead.next
